43d9d99c50375cf3fc1cfd481a0f2066dfc5a922
Paul Jung <porys@hotmail.com>
2017-02-09 11:59:21 +0900

    Second Commit

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import Bot, { Messages } from 'node-line-messaging-api';
 
-const SECRET = process.env.LINE_SECRET || 'f577301fc682b260aff89aa743019f4c'; // Line@ APP SECRET
-const TOKEN = process.env.LINE_TOKEN || 'kzJcqUzg4zRc7plfb84K/CDeI6La+uMuAKeWNKB3RJUAKNpZy2PmCcuBcwtm5EfrDJmCO1CZ5jvV7mWIr132JpIfM8n4RCKgyDYXo87p/ZQogZCO3SiPBc5QVIMMV7l/kdGrMfr28WvAHw3I6Z/LUAdB04t89/1O/w1cDnyilFU='; // Line@ issued TOKEN
+const SECRET = process.env.LINE_SECRET || ''; // Line@ APP SECRET
+const TOKEN = process.env.LINE_TOKEN || ''; // Line@ issued TOKEN
 
 const PORT = process.env.PORT || 3002
+
+console.log('LINE API SETTINGS');
+console.log('SECRET: ' + SECRET);
+console.log('TOKEN: ' + TOKEN);
 
 let bot = new Bot(SECRET, TOKEN, { webhook: { port: PORT } })
 
