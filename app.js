850ed34b2b82339a603d554556517e69569b9deb
Paul Jung <porys@hotmail.com>
2017-02-09 17:21:14 +0900

    last commit

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import Bot, { Messages } from 'node-line-messaging-api'
 
 const SECRET = process.env.LINE_SECRET || '' // Line@ APP SECRET
 const TOKEN = process.env.LINE_TOKEN || '' // Line@ issued TOKEN
-
 const PORT = process.env.PORT || 3002
 
 console.log('LINE API SETTINGS')
@@ -10,13 +9,10 @@ console.log('SECRET: ' + SECRET)
 console.log('TOKEN: ' + TOKEN)
 
 let bot = new Bot(SECRET, TOKEN, { webhook: { port: PORT } })
-
 // bot webhook succesfully started
 bot.on('webhook', w => console.log(`bot listens on port ${w}.`))
-
 // on ANY events
 bot.on('events', e => console.dir(e))
-
 // on Message event
-bot.on('message', m => console.log('m.message')
+bot.on('message', m => console.log(`received message: ${m.message}`)
 );
