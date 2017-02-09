94d1d63db155959bbb277612f0e58dfe75513a58
Paul Jung <porys@hotmail.com>
2017-02-09 11:55:12 +0900

    Second Commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import Bot, { Messages } from 'node-line-messaging-api';
 
-const SECRET = 'f577301fc682b260aff89aa743019f4c' // Line@ APP SECRET
-const TOKEN = 'kzJcqUzg4zRc7plfb84K/CDeI6La+uMuAKeWNKB3RJUAKNpZy2PmCcuBcwtm5EfrDJmCO1CZ5jvV7mWIr132JpIfM8n4RCKgyDYXo87p/ZQogZCO3SiPBc5QVIMMV7l/kdGrMfr28WvAHw3I6Z/LUAdB04t89/1O/w1cDnyilFU=' // Line@ issued TOKEN
+const SECRET = process.env.LINE_SECRET || ''; // Line@ APP SECRET
+const TOKEN = process.env.LINE_SECRET || ''; // Line@ issued TOKEN
 
 const PORT = process.env.PORT || 3002
 
