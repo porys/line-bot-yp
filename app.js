f9fdbdfc12ade00d1303b12f46f778e6a1d69b5f
Paul Jung <porys@hotmail.com>
2017-02-09 13:37:37 +0900

    3

--- app.js
+++ app.js
@@ -20,8 +20,9 @@ bot.on('events', e => console.dir(e))
 // on Message event
 bot.on('message', m =>{
   console.log('received text: ' + m.message.text);
-  console.log('is whereami: ' + m.message.text == 'whereami' + '   ' + m.message.text === 'whereami');
-  if(m.message.text === 'whereami'){
+  console.log('is text: ' + new String(m.message.type) === 'text');
+  console.log('is whereami: ' + m.message.text == 'whereami');
+  if((new String(m.message.type) === 'text') && (new String(m.message.text) === 'whereami')){
     const msgs = new Messages();
     msgs.addText(m.source);
     bot.replyMessage(m.replyToken,msgs.commit());
